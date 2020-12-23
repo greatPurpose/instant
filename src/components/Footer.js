933be31f3169c5beb0856331cf0471ebd9987d3f
greatPurpose <happy.goodday@outlook.com>
2020-12-23 01:31:04 +0100

    mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import config from '../../config';
 import comgraphic from '../assets/images/bg-computer.png';
-import footerSVG from '../assets/images/footer-svg.svg';
+import footerSVG from '../assets/images/footer-svg.png';
 
 
 class FooterComponent extends Component {
@@ -12,7 +12,8 @@ class FooterComponent extends Component {
           <div className="mobile-footer-gh"> 
             <img src={comgraphic}/>            
           </div>         
-          <h2 className="title-footer">{config.mainTitle}<img className="mobile-footer-cosmetic" src={footerSVG}/></h2>
+          <h2 className="title-footer">{config.mainTitle}</h2>
+          <img className="mobile-footer-cosmetic" src={footerSVG}/>
           <div className="btn-link-ft">            
                 <a href="#" target="_blank">{config.btnTitle}</a>
           </div>
